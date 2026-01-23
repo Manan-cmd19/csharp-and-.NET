ffd2bdd6376cc16ebbf61f0140afa72eb5884ae5
Manan <manankalavadiya3@gmail.com>
2026-01-23 22:00:51 +0530

    control-flow
    Today i learn control-flow statement like if , if else, else if , switch

---
+++ control-flow/conditional_statment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class condition_statment
+{
+    static void main()
+    {
+        int a = 1;
+        if (a % 2 = 0;)
+        {
+            
+            Consol.writeLine("a is even number");
+        }
+        else 
+        {
+            Consol.Writeline("a is odd number");
+        }
+    }
+}
